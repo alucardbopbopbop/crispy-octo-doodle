34cf0cc2abe38530dec30793eb7b77d49b436b82
stephen <sevancoe@umich.edu>
2024-03-21 19:39:51 -0700

    working map

--- internship project.py
+++ internship project.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 
 # Read the data from the file
-data = pd.read_excel(io = '/Users/sevancoe/Downloads/Documents/project_form.xlsx')
+data = pd.read_excel(io = '/Users/sevancoe/Downloads/Documents/project_form.xlsx',
+                     dtype = {'revenue': int, 'expenses':int})
+print(data)
+
+map = data.head().values
+print(map)
+
+print(map[0])
+revenue = map[0][1]
+expenses = map[1][1]
+print(revenue)
+print(expenses)
+
+
+#usecols = 'B'
